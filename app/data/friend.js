02fe6dc7ef6e93d0c9b9cf404760a5e505234924
Derek Stewart <derek.charles.stewart@gmail.com>
2019-10-19 12:48:34 -0400

    friendfinder

---
+++ app/data/friend.js
@@ -0,0 +1,89 @@
+//array that holds all friends who have completed the survey
+//initialize with dummy data
+
+
+
+var friendList = [
+    {
+      name: 'Johnny Cash',
+      profilePic: 'https://picsum.photos/200',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+    {
+      name: 'Belinda Bleuminthal',
+      profilePic: 'https://picsum.photos/200',
+      scores: [
+        "1",
+        "1",
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "4",
+        "5",
+        "4"
+      ]
+    },
+    {
+      name: 'Mae-Agness',
+      profilePic: 'https://picsum.photos/200',
+      scores: [
+        "5",
+        "1",
+        "2",
+        "3",
+        "4",
+        "1",
+        "2",
+        "3",
+        "4",
+        "4"
+      ]
+    },
+    {
+      name: 'Alan',
+      profilePic: 'https://picsum.photos/200',
+      scores: [
+        "4",
+        "2",
+        "2",
+        "4",
+        "3",
+        "4",
+        "1",
+        "1",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: 'Pete A. Rouse',
+      profilePic: 'https://picsum.photos/200',
+      scores: [
+        "3",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "3",
+        "1",
+        "1",
+        "1"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
